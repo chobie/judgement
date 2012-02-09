6452a59e4ca90c8d4f2301b4e852f922e246b735
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2012-02-10 03:40:20 +0900

    added in method declraration for brace pracements

--- src/Kuroko/CodeStyleFilter.php
+++ src/Kuroko/CodeStyleFilter.php
@@ -15,6 +15,25 @@ abstract class CodeStyleFilter
 		return new DoubleLinkedListNode(new Token(array(Token::T_NEWLINE,"\n",0)));
 	}
 
+	public function indent($size=1)
+	{
+		switch($this->config["indents.use_tab"]) {
+			case true:
+				$indent = "\t";
+				break;
+			case false:
+				$indent = str_repeat(" ",$this->config["indents.indent"]);
+				break;
+		}
+
+		return new DoubleLinkedListNode(new Token(array(T_WHITESPACE,str_repeat($indent,$size),0)));
+	}
+
+	public function whitespace($size=1)
+	{
+		return new DoubleLinkedListNode(new Token(array(T_WHITESPACE,str_repeat(" ",$size),0)));
+	}
+
 	public function delete(DoubleLinkedListNode $ws)
 	{
 		$previous = $ws->previous;
--- src/Kuroko/CodeStyleFilter/BracesPlacement.php
+++ src/Kuroko/CodeStyleFilter/BracesPlacement.php
@@ -27,6 +27,32 @@ class BracesPlacement extends CodeStyleFilter
 					}
 				break;
 			}
+		} else if ($token->type == T_FUNCTION) {
+			$newline = $this->expectr($node, Token::T_NEWLINE);
+
+			if ($newline->next->data->type == T_WHITESPACE) {
+				// @Todo: detect indent char.
+				$tab = strlen($newline->next->data->data);
+			} else {
+				$tab = 0;
+			}
+
+			$lbrace = $this->expect($node, \Kuroko\Token::T_BRACE_LEFT);
+			$type = strtolower($this->config['braces.in_method_declraration']);
+
+			switch($type) {
+				case "next":
+					if ($lbrace->previous->data->type != Token::T_NEWLINE && $lbrace->previous->data->type != T_WHITESPACE) {
+						$this->inject($this->newline(), $lbrace->previous, $lbrace);
+						$this->inject($this->indent($tab), $lbrace->previous, $lbrace);
+					}
+					break;
+				case "eol":
+					if ($lbrace->previous->data->type == Token::T_NEWLINE) {
+						$this->delete($lbrace->previous);
+					}
+				break;
+			}
 		}
 	}
 }
